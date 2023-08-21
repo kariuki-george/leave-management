4f6edc8055a60a57ff7352ecd419da2b9b5c5807
smiley-geek <kariukigeorge2030@gmail.com>
2023-08-21 07:45:11 +0000

    fix: restricted creating leaveTypes to admins

--- apps/backend/src/leaves/leaveTypes.controller.ts
+++ apps/backend/src/leaves/leaveTypes.controller.ts
@@ -10,14 +10,16 @@ import { LeaveTypesService } from './leaveTypes.service';
 import { ILeaveType } from './models/index.models';
 import { CreateLeaveTypeDto } from './dtos/index.dtos';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/roles/guards/roles.guards';
 
 @Controller('leavetypes')
 export class LeaveTypesController {
   constructor(private readonly leaveTypesService: LeaveTypesService) {}
 
+  // Only the admin can create a leaveType
   @Post()
   @HttpCode(201)
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard, RolesGuard)
   createLeaveType(@Body() input: CreateLeaveTypeDto): Promise<ILeaveType> {
     return this.leaveTypesService.createLeaveType(input);
   }
